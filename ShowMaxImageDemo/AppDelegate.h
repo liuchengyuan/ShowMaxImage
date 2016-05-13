af4e10dfcf3bfebd25065a580b530d35e204d58b
CY L <liuchengyuan@liuchengyuandemacbook-pro.local>
2016-05-13 11:19:21 +0800

    Initial Commit

---
+++ ShowMaxImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowMaxImageDemo
+//
+//  Created by LCY on 16/5/13.
+//  Copyright © 2016年 LCY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShowMaxImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowMaxImageDemo
+//
+//  Created by LCY on 16/5/13.
+//  Copyright © 2016年 LCY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
